bb31008d65917ba997a2a476451fa6e7f8d347e0
Lars K <lars@kroenner.eu>
2020-05-06 01:48:07 +0200

    Initiation of the module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='unimathlib',
+    version='0.0.1',
+    description='My personal universal math lib',
+    url='https://github.com/larsvommars/mathlib',
+    author='LarsVomMars',
+    author_email='dev@kroenner.eu',
+    license='MIT',
+    packages=['unimathlib'],
+    zip_save=False
+)
