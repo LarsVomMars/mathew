27a7c6e82fb861dcbf1d91930f9de7dca54be56d
LarsVomMars <lars@kroenner.eu>
2020-06-21 00:26:17 +0200

    Ready for PyPI

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+
+long_description = open("README.md", "r").read()
+
 setup(
-    name='larsmathlib',
+    name='mathew',
     version='0.0.1',
     description='My personal universal math lib',
     url='https://github.com/larsvommars/mathlib',
     author='LarsVomMars',
     author_email='dev@kroenner.eu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
-    packages=['larsmathlib'],
+    packages=['mathew'],
     zip_save=False
 )
